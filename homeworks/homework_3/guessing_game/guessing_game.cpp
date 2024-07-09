2a9db0e12c6a8fd1d257872a5e052e9aa991dae8
VincentFerrigan <vincent.ferrigan@gmail.com>
2024-07-09 10:18:28 +0200

    Replacing std::endl with '\n'

--- homeworks/homework_3/guessing_game/guessing_game.cpp
+++ homeworks/homework_3/guessing_game/guessing_game.cpp
@@ -4,17 +4,17 @@
 
 auto main() -> int {
     // Print a welcome statement:
-    std::cout << "Welcome to the GUESSING GAME!" << std::endl;
-    std::cout << "I will generate a number and you will guess it!" << std::endl;
+    std::cout << "Welcome to the GUESSING GAME!\n";
+    std::cout << "I will generate a number and you will guess it!\n";
 
     // Ask the user (and read the answer) for the smallest number of the allowed range:
-    std::cout << "Please provide the smallest number:" << std::endl;
+    std::cout << "Please provide the smallest number:\n";
     std::string user_string{};
     std::cin >> user_string;
     int user_smallest_number{std::stoi(user_string)};
 
     // Ask the user (and read the answer) for the largest number of the allowed range:
-    std::cout << "Please provide the largest number:" << std::endl;
+    std::cout << "Please provide the largest number:\n";
     std::cin >> user_string;
     int user_largest_number{std::stoi(user_string)};
 
@@ -22,7 +22,7 @@ auto main() -> int {
     std::random_device random_device;
     std::mt19937 random_engine{random_device()};
     std::uniform_int_distribution distribution{user_smallest_number, user_largest_number};
-    std::cout << "I've generated a number. Try to guess it!" << std::endl;
+    std::cout << "I've generated a number. Try to guess it!\n";
 
     int n{distribution(random_engine)};
     int k{0};
@@ -37,12 +37,12 @@ auto main() -> int {
 
         user_number = std::stoi(user_string);
         if (user_number < n) {
-            std::cout << "Your number is too small. Try again!" << std::endl;
+            std::cout << "Your number is too small. Try again!\n";
         } else if (user_number > n) {
-            std::cout << "Your number is too big. Try again!" << std::endl;
+            std::cout << "Your number is too big. Try again!\n";
         } else { break; }
     }
-    std::cout << "You've done it! You guessed the number " << n << " in " << k << " guesses!" << std::endl;
+    std::cout << "You've done it! You guessed the number " << n << " in " << k << " guesses!\n";
 
     return 0;
 }
